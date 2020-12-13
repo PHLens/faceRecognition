ded4a76605da37a921976db51404440d7079867c
lph <1033957037@qq.com>
2020-12-13 16:31:20 +0800

    modify

--- detect2.py
+++ detect2.py
@@ -6,13 +6,52 @@ import utils.utils as utils
 import insightface
 import datetime
 import hnsw
+import argparse
+
+def prepare(param_file, ctx_id):
+    pos = param_file.rfind('-')
+    prefix = param_file[0:pos]
+    pos2 = param_file.rfind('.')
+    print(pos2)
+    epoch = int(param_file[pos+1:pos2])
+    sym, arg_params, aux_params = mx.model.load_checkpoint(prefix, epoch)
+    all_layers = sym.get_internals()
+    sym = all_layers['fc1_output']
+    if ctx_id>=0:
+        ctx = mx.gpu(ctx_id)
+    else:
+        ctx = mx.cpu()
+    model = mx.mod.Module(symbol=sym, context=ctx, label_names = None)
+    image_size = (112, 112)
+    data_shape = (1,3) + image_size
+    model.bind(data_shapes=[('data', data_shape)])
+    model.set_params(arg_params, aux_params)
+    #warmup
+    data = mx.nd.zeros(shape=data_shape)
+    db = mx.io.DataBatch(data=(data,))
+    model.forward(db, is_train=False)
+    embedding = model.get_outputs()[0].asnumpy()
+    return model
+
+
+def get_embedding(model, img):
+        assert img.shape[2]==3 and img.shape[0:2]==self.image_size
+        data = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        data = np.transpose(data, (2,0,1))
+        data = np.expand_dims(data, axis=0)
+        data = mx.nd.array(data)
+        db = mx.io.DataBatch(data=(data,))
+        model.forward(db, is_train=False)
+        embedding = model.get_outputs()[0].asnumpy()
+        return embedding
 
 class face_rec():
-    def __init__(self):
+    def __init__(self, model):
         self.retinaface_model = insightface.model_zoo.get_model('retinaface_mnet025_v1')
         self.retinaface_model.prepare(ctx_id=0, nms=0.4)
-        self.arcface_model = insightface.model_zoo.get_model('arcface_r100_v1')
-        self.arcface_model.prepare(ctx_id=0)
+        self.arcface_model = model
+        # self.arcface_model = insightface.model_zoo.get_model('arcface_mfn_v1')
+        # self.arcface_model.prepare(ctx_id=0)
         self.p = hnsw.load_index('IJB-C_index.bin', dim=512)
         self.p.set_ef(25)
 
@@ -43,7 +82,7 @@ class face_rec():
             new_img, _ = utils.Alignment_1(crop_img, landmark)
             # new_img = np.expand_dims(new_img,0)
 
-            face_encoding = self.arcface_model.get_embedding(new_img)
+            face_encoding = get_embedding(self.arcface_model, new_img)
             face_encodings.append(face_encoding)
 
         face_names = []
@@ -74,8 +113,27 @@ class face_rec():
 
 
 if __name__ == "__main__":
-    dududu = face_rec()
-    image_path = 'test_data/17488.png'
+    parser = argparse.ArgumentParser(description='do verification')
+    # general
+    parser.add_argument('--data-dir', default='', help='')
+    parser.add_argument('--model',
+                        default='./model/model-y1-test2/model,00',
+                        help='path to load model.')
+    parser.add_argument('--target',
+                        default='./test_data/obama.jpg',
+                        help='test targets.')
+    parser.add_argument('--gpu', default=0, type=int, help='gpu id')
+    parser.add_argument('--batch-size', default=32, type=int, help='')
+    parser.add_argument('--max', default='', type=str, help='')
+    parser.add_argument('--mode', default=0, type=int, help='')
+    parser.add_argument('--nfolds', default=10, type=int, help='')
+    args = parser.parse_args()
+    image_size = [112, 112]
+    print('image_size', image_size)
+    recognition_model = prepare(args.model, args.gpu)
+
+    dududu = face_rec(recognition_model)
+    image_path = args.target
     draw = utils.read_image_gbk(image_path)
     dududu.recognize(draw)
     cv2.imshow('Video', draw)
